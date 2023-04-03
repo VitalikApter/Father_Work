b8ca8b99ad8b91491a516968869514fa616d2439
VitalikApter <aptvetal2014@gmail.com>
2023-04-03 18:56:23 +0300

    Получилось

---
+++ LeaderBoard.js
@@ -0,0 +1,12 @@
+var headerText = `<div class="LeaderBoard_container">
+
+<h2 class="text">LeaderBoard</h2>
+</div>`;
+
+function setHeader() {
+  var header =  document.createElement("div");
+  header.innerHTML = headerText ;
+  document.body.insertAdjacentElement('afterbegin', header );
+}
+
+setHeader();
